ec0e692674d792bc7fdb00deecafc208520f5f76
reemanq <reem.ansar@student.tdsb.on.ca>
2024-11-04 18:45:43 -0500

    Update PP1_7.py

--- PP1_7.py
+++ PP1_7.py
@@ -1,18 +1,41 @@
 
 
 def q1():
+  bool1 = True
+  print(bool1)
   #Write code here
 
 def q2():
+  num = int(input("Input an integer: "))
+  bool1 = num > 5
+  bool1 = True
+  bool1 = False
+  print(bool1)
   #Write code here
 
 def q3():
+  smt = str(input("Input the letter a:"))
+  bool1 = smt == "a"
+  bool1 = True
+  bool1 = False
+  print(bool1)
   #Write code here
 
 def q4():
+  ba = str(input("Input a word earlier in the dictionary than google: ")
+  bool1 = ba > "google"
+  bool1 = True
+  bool1 = False
+  print(bool1)
   #Write code here
 
 def q5():
+  c = int(input("Input an integer: "))
+  a = int(input("Input another integer: "))
+  bool1 = c*a > 40
+  bool1 = True
+  bool1 = False
+  print("Your numbers multiplied together are greater than 40: ",bool1)
   #Write code here
 
 #Do edit the code below
